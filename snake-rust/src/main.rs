66ef01f7deaaa6e43f19cdc06fc15ce91013fccf
CyanUnderscore <106006219+CyanUnderscore@users.noreply.github.com>
2023-04-04 13:03:31 +0200

    init

---
+++ snake-rust/src/main.rs
@@ -0,0 +1,24 @@
+use console::Term;
+
+struct Player {
+    len : i32,
+    color : (i32, i32, i32),
+
+}
+
+fn init() {
+   window  = [[" "; 10] 10] 
+}
+
+
+fn main() {
+    if let Ok(character) = stdout.read_char() {
+            match character {
+                'z' => println!("up"),
+                's' => println!("down"),
+                'q' => println!("left"),
+                'd' => println!("right"),
+                _ => ()
+            }
+        }
+}
